c573d01c0d9d5af4aeafa6bd97adca52b2148ac6
Aleksandr Korbut <akorbut72@gmail.com>
2024-02-24 15:26:36 +0300

    Frst commit

---
+++ Test01_01.py
@@ -0,0 +1,15 @@
+g = 'aaaabbcaa' #input()
+gl = g.lower()
+i = 0
+s = 0
+x = 0
+res = ''
+
+while s < len(gl):
+    j = gl[s]
+    while (len(gl) > i) and  (j == gl[i]):
+        i+=1
+    res += j + str(i-s)
+    s = i 
+print(res)
+print(len(gl))
